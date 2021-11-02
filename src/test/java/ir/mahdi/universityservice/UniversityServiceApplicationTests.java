f59da5bdb899fe60298cc2ed4b3018bf4365a644
mahdi-win10 <riza.rs2000@gmail.com>
2021-11-02 10:33:14 +0330

    init project

---
+++ src/test/java/ir/mahdi/universityservice/UniversityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mahdi.universityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
