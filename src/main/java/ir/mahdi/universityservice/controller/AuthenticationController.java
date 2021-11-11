183185ba5ed06a5b72bfb4568044a28d72d55dbe
mahdi-win10 <riza.rs2000@gmail.com>
2021-11-11 15:14:26 +0330

    add home url to controller

--- src/main/java/ir/mahdi/universityservice/controller/AuthenticationController.java
+++ src/main/java/ir/mahdi/universityservice/controller/AuthenticationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthenticationController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "/home"})
     public String homePage() {
         return "home";
     }
