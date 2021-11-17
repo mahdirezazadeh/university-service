918d8bf42459950757cb867f2e4823f4279ea169
mahdi-win10 <riza.rs2000@gmail.com>
2021-11-17 15:56:46 +0330

    config builder on allArgsConstructor

--- src/main/java/ir/mahdi/universityservice/domain/Exam.java
+++ src/main/java/ir/mahdi/universityservice/domain/Exam.java
@@ -1,8 +1,12 @@
 package ir.mahdi.universityservice.domain;
 
 import ir.mahdi.universityservice.base.BaseEntity;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -13,20 +17,18 @@ import javax.persistence.Table;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Exam extends BaseEntity<Long> {
-//    private final String TITLE = "title";
-//    private final String DESCRIPTION = "description";
-//    private final String DURATION = "duration";
+    private static final String TITLE = "title";
+    private static final String DESCRIPTION = "description";
+    private static final String DURATION = "duration";
 
-
-    //    @Column(name = TITLE)
+    @Column(name = TITLE)
     private String title;
 
-    //    @Column(name = DESCRIPTION)
+    @Column(name = DESCRIPTION)
     private String description;
 
-    //    @Column(name = DURATION)
+    @Column(name = DURATION)
     private int duration = 0;
 
     @ManyToOne
--- src/main/java/ir/mahdi/universityservice/domain/Teacher.java
+++ src/main/java/ir/mahdi/universityservice/domain/Teacher.java
@@ -1,7 +1,7 @@
 package ir.mahdi.universityservice.domain;
 
 import ir.mahdi.universityservice.domain.base.User;
-import lombok.AllArgsConstructor;
+import ir.mahdi.universityservice.domain.enumeration.Gender;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,22 +9,33 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.Set;
 
 
 @Entity
-//@Table(name = Teacher.TABLE_NAME)
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
 @DiscriminatorValue(value = Teacher.DISCRIMINATOR_VALUE)
 public class Teacher extends User {
-    static final String TABLE_NAME = "teacher_table";
     static final String DISCRIMINATOR_VALUE = "TEACHER";
     private static final String TEACHER_CODE = "teacher_code";
 
     @Column(name = TEACHER_CODE)
     private String teacherCode;
+
+    @Builder
+    public Teacher(String userType, @NotNull @NotEmpty(message = "Username can not be empty!") @NotBlank String username, @NotNull @NotEmpty(message = "Username can not be empty!") @NotBlank String password, String firstName, String lastName, Date birthday, String nationalCode, @NotNull @NotEmpty(message = "Username can not be empty!") @NotBlank String email, @NotNull @NotEmpty(message = "Phone Number can not be empty!") @NotBlank String phoneNumber, Gender gender, boolean isActive, boolean isConfirmed, Set<Role> roles, String teacherCode) {
+        super(userType, username, password, firstName, lastName, birthday, nationalCode, email, phoneNumber, gender, isActive, isConfirmed, roles);
+        this.teacherCode = teacherCode;
+    }
+
+    public Teacher(String teacherCode) {
+        this.teacherCode = teacherCode;
+    }
 
     public Teacher() {
         setUserType("Teacher");
